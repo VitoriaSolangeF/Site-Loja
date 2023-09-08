db93498e198ff92bfd8a7aae35214e57b341e8c6
unknown <vitory150518@gmail.com>
2023-09-08 13:55:47 -0300

    loja

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import './Header.css';
+
+
+
+function Header({valor, onchange}) {
+    
+    return (
+        <header className="header">
+            <div className="container_menu">
+                <img className="logo" src="logoloja.png" alt="logo" />
+                <div className="input_search">
+                    <div className="search">
+                        <input 
+                        type="search" 
+                        id="default-search" 
+                        className="busca" 
+                        placeholder="Pesquisar Produtos..." 
+                        value={valor} 
+                        onChange={onchange}
+                        />
+                        <div className="icon-search">
+                            <ion-icon name="search-outline"></ion-icon>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="container_button">
+                    
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
